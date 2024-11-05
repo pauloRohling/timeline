a76055058cf927a159a76999170356fe32e0d63c
Paulo Rohling <paulohr.freecopias@gmail.com>
2024-11-04 22:22:30 -0300

    feat: adding spacing and buttons

--- src/app/timeline/ui/note/note.component.ts
+++ src/app/timeline/ui/note/note.component.ts
@@ -3,16 +3,16 @@ import { TagComponent } from "../../../shared/ui/tag/tag.component";
 import { UserPictureComponent } from "../../../shared/ui/user-picture/user-picture.component";
 import { Note } from "../../../shared/data/note/note.entity";
 import { NgClass } from "@angular/common";
+import {NgIcon} from "@ng-icons/core";
 
 @Component({
   selector: "app-note",
   standalone: true,
-  imports: [TagComponent, UserPictureComponent, NgClass],
+  imports: [TagComponent, UserPictureComponent, NgClass, NgIcon],
   templateUrl: "./note.component.html",
   styleUrl: "./note.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NoteComponent {
-  readonly isReply = input<boolean>(false);
   readonly note = input.required<Note>();
 }
